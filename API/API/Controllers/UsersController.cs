bf16ef70166fee2d1eecc45a9bc3a7afabbf3b82
ehsan bijad <ehsan.bijad90@gmail.com>
2023-01-14 11:45:43 -0500

    End of section 10

--- API/API/Controllers/UsersController.cs
+++ API/API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -28,6 +29,22 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUserByUsername(string username)
         {
             return await _userRepository.GetMemberByUsernameAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            if (user == null) return NotFound();
+
+            _mapper.Map(memberUpdateDto, user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user"); //if send the same data for updating, it returns bad request, because it hasnot changed any data in database
         }
 
     }
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,9 +6,11 @@ import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { PreventUnsavedChangeGuard } from './_guards/prevent-unsaved-change.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -20,7 +22,9 @@ const routes: Routes = [
       { path: 'members', component: MemberListComponent },
       { path: 'members/:username', component: MemberDetailComponent },
       { path: 'messages', component: MessagesComponent },
+      { path: 'member/edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangeGuard] },
       { path: 'lists', component: ListsComponent },
+
     ]
   },
   { path: 'errors', component: TestErrorComponent },
